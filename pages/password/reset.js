32ae4e1b0f1bc91b1913d8f1ce29ff4a166bc527
Dylan Hua <hua.dylan@gmail.com>
2021-02-02 09:29:57 -0600

    news-36
    Fixed css in page for resetting password

--- pages/password/reset.js
+++ pages/password/reset.js
@@ -9,6 +9,17 @@ import MyForm from "../../src/components/view/MyForm";
 import { Button } from 'react-bootstrap';
 import Field from '../../src/components/view/Field';
 import { userServices } from "../../src/services/user/UserService";
+import styled from "styled-components";
+import { styles } from '../../styles/globals';
+
+const header = {
+    children: "Reset Password",
+};
+
+const Container = styled.div`
+    ${styles.form}
+    padding-top: 30vh;
+`;
 
 export default function Reset() {
     const router = useRouter();
@@ -17,69 +28,73 @@ export default function Reset() {
     const [fieldValues, dispatchFields] = useReducer(signUpReducer, initialSignUpState);
     const [loadState, dispatchLoadState] = useReducer(loadReducer, initialLoadState);
 
-    const { successMsgs, errorMsgs, pending } = loadState;
     const { password, cpassword } = fieldValues;
+    const fields = [
+            {
+                label: {
+                    children: "New Password",
+                },
+                input: {
+                    type: "password",
+                    value: password,
+                    onChange: (evt) => dispatchFields(signUpActions.updatePassword(evt.target.value)),
+                }
+            },
+            {
+                label: {
+                    children: "Confirm New Password"
+                },
+                input: {
+                    type: "password",
+                    value: cpassword,
+                    onChange: (evt) => dispatchFields(signUpActions.updateCPassword(evt.target.value)),
+                },
+            },
+        ].map(field => <Field {...field} />);
+
+    const buttons = (
+        <Button 
+            variant={"primary"}
+            block
+            onClick={() => {
+                dispatchLoadState(loadActions.pending());
+                const queryStartString = "?token=";
+                userServices.resetPassword({
+                    ...fieldValues, 
+                    token: asPath.substring(asPath.indexOf(queryStartString) + queryStartString.length)
+                })
+                    .then(() => router.push(pageLink.login))
+                    .catch(error => {
+                        const response = error.response;
+                        if(response && response.status === 400)
+                            dispatchLoadState(loadActions.fail(response.data));
+                        else
+                            dispatchLoadState(loadActions.fail([{message: "There was a problem resetting your password. Please try again."}]));
+                    })
+            }}
+        >
+            Reset Password
+        </Button>
+    );
+
+    const { successMsgs, errorMsgs, pending } = loadState;
+    const messages = {
+        pending: {
+            isPending: pending,
+            message: "Resetting password. Please wait."
+        },
+        success: successMsgs,
+        error: errorMsgs, 
+    };
 
     return (
-        <MyForm 
-            header={{
-                children: "Reset Password",
-            }}
-            fields={[
-                {
-                    label: {
-                        children: "New Password",
-                    },
-                    input: {
-                        type: "password",
-                        value: password,
-                        onChange: (evt) => dispatchFields(signUpActions.updatePassword(evt.target.value)),
-                    }
-                },
-                {
-                    label: {
-                        children: "Confirm New Password"
-                    },
-                    input: {
-                        type: "password",
-                        value: cpassword,
-                        onChange: (evt) => dispatchFields(signUpActions.updateCPassword(evt.target.value)),
-                    },
-                },
-            ].map(field => <Field {...field} />)}
-
-            buttons={
-                <Button 
-                    variant={"primary"}
-                    block
-                    onClick={() => {
-                        dispatchLoadState(loadActions.pending());
-                        const queryStartString = "?token=";
-                        userServices.resetPassword({
-                            ...fieldValues, 
-                            token: asPath.substring(asPath.indexOf(queryStartString) + queryStartString.length)
-                        })
-                            .then(() => router.push(pageLink.login))
-                            .catch(error => {
-                                const response = error.response;
-                                if(response && response.status === 400)
-                                    dispatchLoadState(loadActions.fail(response.data));
-                                else
-                                    dispatchLoadState(loadActions.fail([{message: "There was a problem resetting your password. Please try again."}]));
-                            })
-                    }}
-                >
-                    Reset Password
-                </Button>
-            }
-            messages={{
-                pending: {
-                    isPending: pending,
-                    message: "Resetting password. Please wait."
-                },
-                success: successMsgs,
-                error: errorMsgs, 
-            }}
-        />
-    )
+        <Container>
+            <MyForm 
+                header={header}
+                fields={fields}
+                buttons={buttons}
+                messages={messages}
+            />
+        </Container>
+    );
 }
