6c6b894ac5faa93f262a3939a99c4e6546b00950
Dylan Hua <hua.dylan@gmail.com>
2021-02-02 09:10:27 -0600

    news-31
    Fixed css in page for sending password reset link

--- pages/password/email.js
+++ pages/password/email.js
@@ -5,63 +5,77 @@ import MyForm from "../../src/components/view/MyForm";
 import Field from "../../src/components/view/Field";
 import { Button } from "react-bootstrap";
 import loadActions from "../../src/redux/actions/loadAction";
+import { styles } from "../../styles/globals";
+import styled from "styled-components";
+
+const header = {
+    children: "Reset Password"
+};
+
+const Container = styled.div`
+    ${styles.form}
+    padding-top: 30vh;
+`;
 
 export default function Email() {
     const [loadState, dispatchLoadState] = useReducer(loadReducer, initialLoadState);
     const [email, setEmail] = useState("");
 
-    const { pending, errorMsgs, successMsgs } = loadState;
-
-    return (
-        <MyForm
-            header={{
-                children: "Reset Password"
-            }}
-            fields={[
-                {
-                    label: {
-                        children: "Email",
-                    },
-                    input: {
-                        placeholder: "abc@domain.com",
-                        value: email,
-                        onChange: (evt) => setEmail(evt.target.value),
-                    }
-                }
-            ].map(field => <Field {...field} />)}
-
-            buttons={
-                <Button 
-                    variant="primary"
-                    block
-                    onClick={() => {
-                        dispatchLoadState(loadActions.pending());
-                        userServices
-                            .sendEmail({email: email})
-                            .then(success => dispatchLoadState(loadActions.success([success.data])))
-                            .catch(error => {
-                                const response = error.response;
-                                if(response && response.status === 400) {
-                                    dispatchLoadState(loadActions.fail([response.data]));
-                                } else {
-                                    dispatchLoadState(loadActions.fail([{
-                                        message: "There was a problem sending the link. Please try again."
-                                    }]));
-                                }
-                            })
-                    }}
-                >
-                    Send Email
-                </Button>
-            }
-            messages={{
-                pending: {
-                    isPending: pending,
-                    message: "Sending link to reset password. Please wait."
+    const fields = [
+            {
+                label: {
+                    children: "Email",
                 },
-                success: successMsgs,
-                error: errorMsgs,
+                input: {
+                    placeholder: "abc@domain.com",
+                    value: email,
+                    onChange: (evt) => setEmail(evt.target.value),
+                }
+            }
+        ].map(field => <Field {...field} />);
+
+    const buttons = (
+        <Button 
+            variant="primary"
+            block
+            onClick={() => {
+                dispatchLoadState(loadActions.pending());
+                userServices
+                    .sendEmail({email: email})
+                    .then(success => dispatchLoadState(loadActions.success([success.data])))
+                    .catch(error => {
+                        const response = error.response;
+                        if(response && response.status === 400) {
+                            dispatchLoadState(loadActions.fail([response.data]));
+                        } else {
+                            dispatchLoadState(loadActions.fail([{
+                                message: "There was a problem sending the link. Please try again."
+                            }]));
+                        }
+                    })
             }}
-        />
+        >
+            Send Email
+        </Button>
+    );
+
+    const { pending, errorMsgs, successMsgs } = loadState;
+    const messages = {
+        pending: {
+            isPending: pending,
+            message: "Sending link to reset password. Please wait."
+        },
+        success: successMsgs,
+        error: errorMsgs,
+    };
+    return (
+        <Container>
+            <MyForm
+                header={header}
+                fields={fields}
+                buttons={buttons}
+                messages={messages}
+            />
+        </Container>
     );
 }
--- src/components/view/SuccessMessage.js
+++ src/components/view/SuccessMessage.js
@@ -4,15 +4,16 @@ import { styles } from "../../../styles/globals";
 import { IconMessage } from "./IconMessage";
 
 const Icon = styled(CheckCircle)`${styles.iconMessage}`;
+const msgProps = {
+    backgroundColor: "#62FF83",
+    textColor: "green"
+};
 
-export function SuccessMessage({ message }) {
+export function SuccessMessage({ props=msgProps, icon=<Icon />, message="" }) {
     return (
         <IconMessage 
-            props={{
-                backgroundColor: "#62FF83",
-                textColor: "green"
-            }}
-            icon={<Icon />}
+            props={props}
+            icon={icon}
             message={message}
         />
     );
