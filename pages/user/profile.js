30a2f3199081a802881c1f43b37098ce4bc2c5e5
Dylan Hua <hua.dylan@gmail.com>
2021-02-03 13:53:58 -0600

    news-34
    Fixed css in profile page

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -14,17 +14,24 @@ import useSWR from "swr";
 import { apiLink, pageLink } from "../../src/constants";
 import ConfirmationPrompt from "../../src/components/view/ConfirmationPrompt";
 import { useRouter } from 'next/router';
+import styled from "styled-components";
+import { styles } from "../../styles/globals";
+import profileReducer, { initialProfileState } from "../../src/redux/reducers/profileReducer";
+import profileActions from "../../src/redux/actions/profileAction";
 
 const fetcher = (url) => userServices.getProfile();
+const Container = styled.div`
+    ${styles.form}
+    padding-top: 20vh;
+    padding-left: 100px;
+`;
 
 export default function Profile() {
     const [loadState, dispatchLoadState] = useReducer(loadReducer, initialLoadState);
     const [fieldValues, dispatchFields] = useReducer(signUpReducer, initialSignUpState);
-    const [showAskPrompt, setShowAskPrompt] = useState(false);
-    const [deleteError, setDeleteError] = useState(null);
-    const router = useRouter();
+    const [profileState, dispatchProfileState] = useReducer(profileReducer, initialProfileState);
 
-    const { successMsgs, errorMsgs, pending } = loadState;
+    const router = useRouter();
 
     const { username, email } = fieldValues;
     const { data, error } = useSWR(apiLink.getProfile, fetcher);
@@ -41,124 +48,155 @@ export default function Profile() {
     } else if (!data) {
         componentToRender = <Loader />;
     } else {        
+        const header = {
+            children: "Profile"
+        };
+
+        const { successMsgs, errorMsgs, pending } = loadState;
+
+        const fields = [
+                {
+                    label: {
+                        children: "Username"
+                    },
+                    input: {
+                        value: username,
+                        onChange: (evt) => dispatchFields(signUpActions.updateUsername(evt.target.value))
+                    },
+                    successMsgs: successMsgs.username_success,
+                    errorMsgs: successMsgs.username_error,
+                },
+                {
+                    label: {
+                        children: "Email"
+                    },
+                    input: {
+                        type: "email",
+                        value: email,
+                        onChange: (evt) => dispatchFields(signUpActions.updateEmail(evt.target.value))
+                    },
+                    successMsgs: successMsgs.email_success,
+                    errorMsgs: successMsgs.email_error,
+                }
+            ].map((field, index) => (
+                <div key={index}>
+                    <Field {...field} />
+                </div>
+            ));
+
+        const buttons = [
+                {
+                    props: {
+                        variant: "primary",
+                        onClick: () => {
+                            dispatchLoadState(loadActions.pending());
+                            userServices.updateProfile(fieldValues)
+                                .then(success => dispatchLoadState(loadActions.success(success.data)))
+                                .catch(() => dispatchLoadState(loadActions.fail(["There was a problem updating your profile. Please try again."])))
+                        }
+                    },
+                    children: "Update profile"
+                }
+            ].map((button, index) => {
+                const { props, children } = button;
+                return (
+                    <Button {...props} block key={index}>
+                        {children}
+                    </Button>
+                );
+            });
+            
+        const messages = {
+            success: [],
+            error: errorMsgs,
+            pending: {
+                isPending: pending,
+                message: "Updating your profile. Please wait." 
+            }
+        };
+
         componentToRender = (
             <MyForm
-                header={{
-                    children: "Profile",
-                    textAlign: "left",
-                }}
-                fields={[
-                    {
-                        label: {
-                            children: "Username"
-                        },
-                        input: {
-                            value: username,
-                            onChange: (evt) => dispatchFields(signUpActions.updateUsername(evt.target.value))
-                        },
-                        successMsgs: successMsgs.username_success,
-                        errorMsgs: successMsgs.username_error,
-                    },
-                    {
-                        label: {
-                            children: "Email"
-                        },
-                        input: {
-                            type: "email",
-                            value: email,
-                            onChange: (evt) => dispatchFields(signUpActions.updateEmail(evt.target.value))
-                        },
-                        successMsgs: successMsgs.email_success,
-                        errorMsgs: successMsgs.email_error,
-                    }
-                ].map((field, index) => (
-                    <div key={index}>
-                        <Field {...field} />
-                    </div>
-                ))}
-
-                buttons={[
-                    {
-                        props: {
-                            variant: "primary",
-                            onClick: () => {
-                                dispatchLoadState(loadActions.pending());
-                                userServices.updateProfile(fieldValues)
-                                    .then(success => dispatchLoadState(loadActions.success(success.data)))
-                                    .catch(() => dispatchLoadState(loadActions.fail(["There was a problem updating your profile. Please try again."])))
-                            }
-                        },
-                        children: "Update profile"
-                    }
-                ].map((button, index) => {
-                    const { props, children } = button;
-                    return (
-                        <Button {...props} block key={index}>
-                            {children}
-                        </Button>
-                    );
-                })}
-                messages={{
-                    success: [],
-                    error: errorMsgs,
-                    pending: {
-                        isPending: pending,
-                        message: "Updating your profile. Please wait." 
-                    }
-                }}
+                header={header}
+                fields={fields}
+                buttons={buttons}
+                messages={messages}
             />);
     }
+
+    const { showAskPrompt, deleteError } = profileState;
+
+    const modal = {
+        show: showAskPrompt,
+        onHide: () => dispatchProfileState(profileActions.showAskPrompt(false)),
+    };
+
+    const header = {
+        children: "Delete Account?",
+    };
+
+    const body = {
+        children: <>
+            Are you sure you want to delete your account? You cannot undo this.
+            {
+                loadState.pending &&
+                    <Loader message={"Deleting your account..."} />
+            }
+            {
+                deleteError && 
+                    <ErrorMessage
+                        message={"There was a problem deleting your account. Please try again."}
+                    />
+            }
+        </>
+    };
+
+    const footer = {
+        children: [
+                {
+                    props: {
+                        variant: "primary",
+                        onClick: () => dispatchProfileState(profileActions.showAskPrompt(false)),
+                    },
+                    children: "Cancel"
+                },
+                {
+                    props: {
+                        variant: "danger",
+                        onClick: () => {
+                            dispatchLoadState(loadActions.pending());
+                            userServices
+                                .deleteAccount()
+                                .then(() => router.push(pageLink.home))
+                                .catch(error => dispatchProfileState(profileActions.deleteError(error.response)))
+                        },
+                    },
+                    children: "Delete Account"
+                },
+            ].map(button => <Button {...button.props}>{button.children}</Button>)
+    };
 
     return (
         <>
             <Sidenav />
-            {componentToRender}
-            <h1>Delete Account</h1>
-            <p>You cannot undo this action. Please be sure about this.</p>
-            <Button variant="danger" onClick={() => setShowAskPrompt(true)}>
-                Delete Account
-            </Button>
-            <ConfirmationPrompt
-                modal={{
-                    show: showAskPrompt,
-                    onHide: () => setShowAskPrompt(false),
-                }}
-                header={{
-                    children: "Delete Account?"
-                }}
-                body={{
-                    children: <>
-                        Are you sure you want to delete your account? You cannot undo this.
-                        {
-                            deleteError && 
-                                <ErrorMessage
-                                    message={"There was a problem deleting your account. Please try again."}
-                                />
-                        }
-                    </>
-                }}
-                footer={{
-                    children: [
-                        {
-                            props: {
-                                variant: "primary",
-                                onClick: () => setShowAskPrompt(false),
-                            },
-                            children: "Cancel"
-                        },
-                        {
-                            props: {
-                                variant: "danger",
-                                onClick: () => userServices
-                                    .deleteAccount()
-                                    .then(() => router.push(pageLink.home))
-                                    .catch(error => setDeleteError(error.response)),
-                            },
-                            children: "Delete Account"
-                        },
-                    ].map(button => <Button {...button.props}>{button.children}</Button>)
-                }}
-            />
+            <Container>
+                {componentToRender}
+                <h2>Delete Account</h2>
+                <hr />
+                <p>You cannot undo this action. Please be sure about this.</p>
+                <Button 
+                    variant="danger" 
+                    onClick={() => dispatchProfileState(profileActions.showAskPrompt(true))}
+                >
+                    Delete Account
+                </Button>
+                <ConfirmationPrompt
+                    modal={modal}
+                    header={header}
+                    body={body}
+                    footer={footer}
+                />
+            </Container>
         </>
     );
 }
