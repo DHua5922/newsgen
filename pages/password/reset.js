4eedba230e17a1634cf34989531cd25db0c9b645
Dylan Hua <hua.dylan@gmail.com>
2021-04-04 11:01:18 -0500

    CU-r76jh1 - Revamp new password UI (#28)

--- pages/password/reset.js
+++ pages/password/reset.js
@@ -1,100 +1,118 @@
 import { useRouter } from 'next/router';
-import { useReducer } from 'react';
 import { pageLink } from '../../src/constants';
-import signUpReducer, { initialSignUpState } from "../../src/redux/reducers/signUpReducer";
 import signUpActions from "../../src/redux/actions/signUpAction";
-import loadReducer, { initialLoadState } from "../../src/redux/reducers/loadReducer";
 import loadActions from "../../src/redux/actions/loadAction";
 import MyForm from "../../src/components/view/MyForm";
 import { Button } from 'react-bootstrap';
 import Field from '../../src/components/view/Field';
 import { userServices } from "../../src/services/user/UserService";
-import styled from "styled-components";
-import { styles } from '../../styles/globals';
+import WithTitle from '../../src/components/view/WithTitle';
+import WithNavbar from '../../src/components/view/WithNavbar';
+import AuthForm from '../../src/components/view/AuthForm';
+import { useDispatch, useSelector } from "react-redux";
+import useMessages from '../../src/custom-hooks/useMessages';
 
 const header = {
     children: "Reset Password",
 };
 
-const Container = styled.div`
-    ${styles.form}
-    padding-top: 30vh;
-`;
+function useFields() {
+    const { password, cpassword } = useSelector(state => state.signUpReducer);
+    const dispatch = useDispatch();
 
-export default function Reset() {
+    return [
+        {
+            label: {
+                children: "New Password",
+            },
+            input: {
+                type: "password",
+                value: password,
+                onChange: (evt) => dispatch(signUpActions.updatePassword(evt.target.value)),
+            }
+        },
+        {
+            label: {
+                children: "Confirm New Password"
+            },
+            input: {
+                type: "password",
+                value: cpassword,
+                onChange: (evt) => dispatch(signUpActions.updateCPassword(evt.target.value)),
+            },
+        },
+    ];
+}
+
+function useButtons() {
+    const fieldValues = useSelector(state => state.signUpReducer);
+    const dispatch = useDispatch();
     const router = useRouter();
     const { asPath } = router;
 
-    const [fieldValues, dispatchFields] = useReducer(signUpReducer, initialSignUpState);
-    const [loadState, dispatchLoadState] = useReducer(loadReducer, initialLoadState);
+    function resetPassword() {
+        dispatch(loadActions.pending());
+        const queryStartString = "?token=";
+        userServices.resetPassword({
+            ...fieldValues, 
+            token: asPath.substring(asPath.indexOf(queryStartString) + queryStartString.length)
+        })
+            .then(() => {
+                dispatch(loadActions.success([]));
+                router.push(pageLink.login);
+            })
+            .catch(error => {
+                const response = error.response;
+                if(response && response.status === 400)
+                    dispatch(loadActions.fail(response.data));
+                else
+                    dispatch(loadActions.fail([{message: "There was a problem resetting your password. Please try again."}]));
+            })
+    }
 
-    const { password, cpassword } = fieldValues;
-    const fields = [
-            {
-                label: {
-                    children: "New Password",
-                },
-                input: {
-                    type: "password",
-                    value: password,
-                    onChange: (evt) => dispatchFields(signUpActions.updatePassword(evt.target.value)),
-                }
+    return [
+        {
+            props: {
+                variant: "primary",
+                onClick: () => resetPassword()
             },
-            {
-                label: {
-                    children: "Confirm New Password"
-                },
-                input: {
-                    type: "password",
-                    value: cpassword,
-                    onChange: (evt) => dispatchFields(signUpActions.updateCPassword(evt.target.value)),
-                },
-            },
-        ].map(field => <Field {...field} />);
+            children: "Reset Password"
+        }
+    ];
+}
 
-    const buttons = (
-        <Button 
-            variant={"primary"}
-            block
-            onClick={() => {
-                dispatchLoadState(loadActions.pending());
-                const queryStartString = "?token=";
-                userServices.resetPassword({
-                    ...fieldValues, 
-                    token: asPath.substring(asPath.indexOf(queryStartString) + queryStartString.length)
-                })
-                    .then(() => router.push(pageLink.login))
-                    .catch(error => {
-                        const response = error.response;
-                        if(response && response.status === 400)
-                            dispatchLoadState(loadActions.fail(response.data));
-                        else
-                            dispatchLoadState(loadActions.fail([{message: "There was a problem resetting your password. Please try again."}]));
-                    })
-            }}
-        >
-            Reset Password
-        </Button>
-    );
+function MainContent() {
+    const fields = useFields().map((field, index) => (
+        <div key={index}>
+            <Field {...field} />
+        </div>
+    ));
 
-    const { successMsgs, errorMsgs, pending } = loadState;
-    const messages = {
-        pending: {
-            isPending: pending,
-            message: "Resetting password. Please wait."
-        },
-        success: successMsgs,
-        error: errorMsgs, 
-    };
+    const buttons = useButtons().map((button, index) => {
+        const { props, children } = button;
+        return (
+            <Button {...props} block key={index}>
+                {children}
+            </Button>
+        );
+    });
+
+    const { pending, errorMsgs, successMsgs } = useSelector(state => state.loadReducer);
+    const messages = useMessages(successMsgs, errorMsgs, pending, "Resetting password. Please wait.");
 
     return (
-        <Container>
-            <MyForm 
-                header={header}
-                fields={fields}
-                buttons={buttons}
-                messages={messages}
-            />
-        </Container>
+        <MyForm
+            header={header}
+            fields={fields}
+            buttons={buttons}
+            messages={messages}
+        />
     );
 }
+
+function Reset() {
+    const EmailPage = WithTitle(WithNavbar(AuthForm(MainContent)));
+    return <EmailPage title={"Reset Password"} />;
+}
+
+export default Reset;
